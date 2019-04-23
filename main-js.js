e37e73a73b885b8bfd5e436a737bbc2cd423858d
appledoes <48528133+appledoes@users.noreply.github.com>
2019-04-23 10:40:48 +0100

    v1.0 alpha snapshot 1.0

--- main-js.js
+++ main-js.js
@@ -30,8 +30,8 @@ const importPage = function (page)
 
     xhr1.onload = function ()
     {
-        if (xhr1.status === 200)
-        {
+        //if (xhr1.status === 200)
+        //{
             document.getElementById('mn-content').innerHTML = xhr1.response;
             if (page === 'index') {
                 // Login
@@ -42,35 +42,46 @@ const importPage = function (page)
                     explanation.addClass('hidden');
                     $topic.text('Welcome back ' + ls.login + '!');
                 };
-            }
-        }
+            };
+        //}
         
     };
 
     xhr2.onload = function ()
     {
-        if (xhr2.status === 200)
-        {
+        //if (xhr2.status === 200)
+        //{
             responseObject = JSON.parse(xhr2.response);
 
             var $exp_h3 = $('.explanation h3');
             var $exp_h4 = $('.explanation h4');
 
+            var $a1_h3 = $('.a1-header');
+            var $a1_p1 = $('.a1-p1');
+            var $a1_p2 = $('.a1-p2');
+            var $a1_p3 = $('.a1-p3');
+            var $a1_p4 = $('.a1-p4');
+            var $a1_p5 = $('.a1-p5');
+
+            var $ns1_h3 = $('.ns1-header');
+            var $ns1_p1 = $('.ns1-p1');
+            var $ns1_p2 = $('.ns1-p2');
+            var $ns1_p3 = $('.ns1-p3');
+
             $exp_h3.text(responseObject.topic[0].t_exp);
             $exp_h4.text(responseObject.topic[1].b_exp);
 
-            if (responseObject.topic[0].weight == "bold") {
-                $exp_h3.addClass('bold');
-            } else {
-                $exp_h3.addClass('light');
-            };
+            $a1_h3.text(responseObject.articles[0].header);
+            $a1_p1.text(responseObject.articles[0].p1);
+            $a1_p2.text(responseObject.articles[0].p2);
+            $a1_p3.text(responseObject.articles[0].p3);
+            $a1_p4.text(responseObject.articles[0].p4);
 
-            if (responseObject.topic[1].weight == "bold") {
-                $exp_h4.addClass('bold');
-            } else {
-                $exp_h4.addClass('light');
-            };
-        }
+            $ns1_h3.text(responseObject.news[0].header);
+            $ns1_p1.text(responseObject.news[0].p1);
+            $ns1_p2.text(responseObject.news[0].p2)
+        //}
+        
     };
 
     xhr1.open('GET', 'htmlBase.html', true);
