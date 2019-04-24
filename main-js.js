7867d6b456909031cd54737aa88b84df693993c0
appledoes <48528133+appledoes@users.noreply.github.com>
2019-04-24 17:54:32 +0100

    v1.0 alpha snapshot 5

--- main-js.js
+++ main-js.js
@@ -6,6 +6,7 @@
 // [[ Title ]] //
 var $title = $('title').text();
 console.log('Current page: ' + $title);
+console.log('SNAPSHOT 4')
 
 // [[ Variables ]] //
 
@@ -47,8 +48,7 @@ const importPage = function (page)
         
     };
 
-    xhr1.open('GET', 'htmlBase.html', true);
-    xhr1.send(null);
+
 
     xhr2.onload = function ()
     {
@@ -61,35 +61,74 @@ const importPage = function (page)
 
 
         
-            var $a1_ul = $('.a1-ul');
             var $a1_h3 = $('.a1-header');
+            var $a4_h3 = $('.a4-header');
 
-            function ar(article, type, number)
+            var $ns1_h3 = $('.ns1-header')
+
+            function ar(article, type, number, articleList, display)
             {
                 this.article = article;
                 this.type = type;
                 this.number = number;
+                this.articleList = articleList
+                this.display = display
                 this.makeli = function ()
                 {
                     var makeNew = this.article + '_' + this.type + this.number;
-                    var newliItem = '<li><p class="' + makeNew + '"></p></li>'
-                    
-                    $('.a1-ul').append(newliItem);
+                    var newliItem = '<li class="' + this.display + '"><' + this.type + ' class="' + makeNew + '"></' + this.type + '></li>'
+
+                    $(this.articleList).append(newliItem);
+                    console.log(newliItem)
+
+
                 }
             }
 
+            for (i = 1; i < 5; i++)
+            {
+                var makeNewli = new ar('a1', 'p', i, '.a1-ul', 'block');
 
-            for (i = 1; i > 11; i++) {
-                var makeNewli = new ar('a1', 'p', i)
-                makeNewli.makeli()
-                
-             }
+                makeNewli.makeli();
+            }
+
+            for (i = 1; i < 3; i++)
+            {
+                var makeNewli = new ar('a4', 'p', i, '.a4-ul', 'block');
+
+                makeNewli.makeli();
+            }
+
+            for (i = 1; i < 3; i++)
+            {
+                var makeNewli = new ar('ns1', 'p', i, '.ns1-ul', 'inline')
+
+                makeNewli.makeli();
+            }
+
+            $('.a1_p1').text(responseObject.articles[0].p1);
+            $('.a1_p2').text(responseObject.articles[0].p2);
+            $('.a1_p3').text(responseObject.articles[0].p3);
+            $('.a1_p4').text(responseObject.articles[0].p4);
+
+            $('.a4_p1').text(responseObject.articles[1].p1);
+            $('.a4_p2').text(responseObject.articles[1].p2);
+
+            $('.ns1_p1').text(responseObject.news[0].p1);
+            $('.ns1_p2').text(responseObject.news[0].p2);
 
             $exp_h3.text(responseObject.topic[0].t_exp);
             $exp_h4.text(responseObject.topic[1].b_exp);
+
+            $a1_h3.text(responseObject.articles[0].header);
+            $a4_h3.text(responseObject.articles[1].header);
+            $ns1_h3.text(responseObject.news[0].header);
         //}
         
     };
+    xhr1.open('GET', 'htmlBase.html', true);
+    xhr1.send(null);
+
     xhr2.open('GET', 'json/json1-index.json', true);
     xhr2.send(null);
 }
