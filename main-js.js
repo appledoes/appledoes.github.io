ea4260e47123313afec30fcbdba8f56ae6ec8717
appledoes <48528133+appledoes@users.noreply.github.com>
2019-05-02 22:05:33 +0100

    alpha 1.0 snapshot 6

--- main-js.js
+++ main-js.js
@@ -1,7 +1,20 @@
-// main-js.js //
+﻿// main-js.js //
 // Only to be used for pwManager //
 
 // Uses jQuery //
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBnrAhY_w3VVnjHc2_UeJJAt2G-FWpAjwM",
+    authDomain: "pwmanager-91c7e.firebaseapp.com",
+    databaseURL: "https://pwmanager-91c7e.firebaseio.com",
+    projectId: "pwmanager-91c7e",
+    storageBucket: "pwmanager-91c7e.appspot.com",
+    messagingSenderId: "200620870742"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
 
 // [[ Title ]] //
 var $title = $('title').text();
@@ -62,9 +75,16 @@ const importPage = function (page)
 
         
             var $a1_h3 = $('.a1-header');
+            var $a2_h3 = $('.a2-header');
+            var $a3_h3 = $('.a3-header');
             var $a4_h3 = $('.a4-header');
-
-            var $ns1_h3 = $('.ns1-header')
+            var $a5_h3 = $('.a5-header');
+            var $a6_h3 = $('.a6-header');
+            var $a7_h3 = $('.a7-header');
+            var $a8_h3 = $('.a8-header');
+       
+            var $ns1_h3 = $('.ns1-header');
+            var $ns2_h3 = $('.ns2-header');
 
             function ar(article, type, number, articleList, display)
             {
@@ -76,53 +96,141 @@ const importPage = function (page)
                 this.makeli = function ()
                 {
                     var makeNew = this.article + '_' + this.type + this.number;
-                    var newliItem = '<li class="' + this.display + '"><' + this.type + ' class="' + makeNew + '"></' + this.type + '></li>'
+                    var newliItem = '<li class="' + this.display + '"><' + this.type + ' class="' + makeNew + '"></' + this.type + '></li>';
 
                     $(this.articleList).append(newliItem);
-                    console.log(newliItem)
+                    console.log(newliItem);
 
 
-                }
-            }
+                };
+            };
 
             for (i = 1; i < 5; i++)
             {
                 var makeNewli = new ar('a1', 'p', i, '.a1-ul', 'block');
 
-                makeNewli.makeli();
-            }
+                makeNewli.makeli()
+            };
 
             for (i = 1; i < 3; i++)
             {
-                var makeNewli = new ar('a4', 'p', i, '.a4-ul', 'block');
+                var makeNewli = new ar('a2', 'p', i, '.a2-ul', 'block');
 
                 makeNewli.makeli();
-            }
+            };
 
             for (i = 1; i < 3; i++)
             {
-                var makeNewli = new ar('ns1', 'p', i, '.ns1-ul', 'inline')
+                var makeNewli = new ar('ns1', 'p', i, '.ns1-ul', 'inline');
 
                 makeNewli.makeli();
-            }
+            };
+
+            for (i = 1; i < 3; i++)
+            {
+                var makeNewli = new ar('ns2', 'p', i, '.ns2-ul', 'inline');
+
+                makeNewli.makeli();
+            };
+
+            if (responseObject.articles[0].header == undefined)
+            {
+                console.log('working ([0] li)');
+                $a1_h3.text('Sorry');
+                $('.a1-ul').replaceWith('<p>Sorry, This article has not been written yet</p><br /> <p>😢</p>');
+                $('.article-1').css({
+                    'text-align': 'center'
+                });
+            };
+
+            if (responseObject.articles[1].header == undefined) {
+                console.log('working ([1] li)');
+                $a2_h3.text('Sorry');
+                $('.a2-ul').replaceWith('<p>Sorry, This article has not been written yet</p><br /> <p>😢</p>')
+                $('.article-2').css({
+                    'text-align': 'center'
+                });
+            };
+
+            if (responseObject.articles[2].header == undefined) {
+                console.log('working ([2] li)');
+                $a3_h3.text('Sorry');
+                $('.a3-ul').replaceWith('<p>Sorry, This article has not been written yet</p><br /> <p>😢</p>')
+                $('.article-3').css({
+                    'text-align': 'center'
+                });
+            };
+
+            if (responseObject.articles[3].header == undefined) {
+                console.log('working ([3] li)');
+                $a4_h3.text('Sorry');
+                $('.a4-ul').replaceWith('<p>Sorry, This article has not been written yet</p><br /> <p>😢</p>')
+                $('.article-4').css({
+                    'text-align': 'center'
+                });
+            };
+
+            if (responseObject.articles[4].header == undefined) {
+                console.log('working ([4] li)');
+                $a5_h3.text('Sorry');
+                $('.a5-ul').replaceWith('<p>Sorry, This article has not been written yet</p><br /> <p>😢</p>');
+                $('.article-5').css({
+                    'text-align': 'center'
+                });
+            };
+
+            if (responseObject.articles[5].header == undefined) {
+                console.log('working ([5] li)');
+                $a6_h3.text('Sorry');
+                $('.a6-ul').replaceWith('<p>Sorry, This article has not been written yet</p><br /> <p>😢</p>')
+                $('.article-6').css({
+                    'text-align': 'center'
+                });
+            };
+
+            if (responseObject.articles[6].header == undefined) {
+                console.log('working ([6] li)');
+                $a7_h3.text('Sorry');
+                $('.a7-ul').replaceWith('<p>Sorry, This article has not been written yet</p><br /> <p>😢</p>')
+                $('.article-7').css({
+                    'text-align': 'center'
+                });
+            };
+
+            if (responseObject.articles[7].header == undefined) {
+                console.log('working ([7] li)');
+                $a8_h3.text('Sorry');
+                $('.a8-ul').replaceWith('<p>Sorry, This article has not been written yet</p><br /> <p>😢</p>')
+                $('.article-8').css({
+                    'text-align': 'center'
+                });
+            };
+
 
             $('.a1_p1').text(responseObject.articles[0].p1);
             $('.a1_p2').text(responseObject.articles[0].p2);
             $('.a1_p3').text(responseObject.articles[0].p3);
             $('.a1_p4').text(responseObject.articles[0].p4);
 
-            $('.a4_p1').text(responseObject.articles[1].p1);
-            $('.a4_p2').text(responseObject.articles[1].p2);
+            $('.a2_p1').text(responseObject.articles[1].p1);
+            $('.a2_p2').text(responseObject.articles[1].p2);
 
             $('.ns1_p1').text(responseObject.news[0].p1);
             $('.ns1_p2').text(responseObject.news[0].p2);
 
+            $('.ns2_p1').text(responseObject.news[1].p1);
+            $('.ns2_p2').text(responseObject.news[1].p2);
+
             $exp_h3.text(responseObject.topic[0].t_exp);
             $exp_h4.text(responseObject.topic[1].b_exp);
 
+            $('.whr1_header').text(responseObject.whr[0].header);
+            $('.whr1_desc').text(responseObject.whr[0].desc);
+       
             $a1_h3.text(responseObject.articles[0].header);
-            $a4_h3.text(responseObject.articles[1].header);
+            $a2_h3.text(responseObject.articles[1].header);
             $ns1_h3.text(responseObject.news[0].header);
+            $ns2_h3.text(responseObject.news[1].header);
         //}
         
     };
