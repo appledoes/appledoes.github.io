8ddc41c37845afe6c4b262e69076948357b0706f
appledoes <48528133+appledoes@users.noreply.github.com>
2019-05-04 13:55:16 +0100

    alpha 1.2 snapshot 1w1

--- account/signup-fireBase.js
+++ account/signup-fireBase.js
@@ -1,1 +1,19 @@
 // signup-fireBase.js //
+
+var app_fireBase = {};
+
+(function () {
+    // Initialize Firebase
+
+    var config = {
+        apiKey: "AIzaSyBnrAhY_w3VVnjHc2_UeJJAt2G-FWpAjwM",
+        authDomain: "pwmanager-91c7e.firebaseapp.com",
+        databaseURL: "https://pwmanager-91c7e.firebaseio.com",
+        projectId: "pwmanager-91c7e",
+        storageBucket: "pwmanager-91c7e.appspot.com",
+        messagingSenderId: "200620870742"
+    };
+    firebase.initializeApp(config);
+
+    app_fireBase = firebase;
+})()
--- main-js.js
+++ main-js.js
@@ -2,29 +2,12 @@
 // Only to be used for pwManager //
 
 // Uses jQuery //
-var app_fireBase = {};
-
-(function () {
-    // Initialize Firebase
-
-    var config = {
-        apiKey: "AIzaSyBnrAhY_w3VVnjHc2_UeJJAt2G-FWpAjwM",
-        authDomain: "pwmanager-91c7e.firebaseapp.com",
-        databaseURL: "https://pwmanager-91c7e.firebaseio.com",
-        projectId: "pwmanager-91c7e",
-        storageBucket: "pwmanager-91c7e.appspot.com",
-        messagingSenderId: "200620870742"
-    };
-    firebase.initializeApp(config);
-
-    app_fireBase = firebase;
-})()
 
 
 // [[ Title ]] //
 var $title = $('title').text();
 console.log('Current page: ' + $title);
-console.log('SNAPSHOT 1')
+console.log('SNAPSHOT 1w1')
 
 // [[ Variables ]] //
 
