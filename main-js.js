4a6ef44e2f1ade5bcdeff362d098cd2d25f61d6a
appledoes <48528133+appledoes@users.noreply.github.com>
2019-05-04 22:02:34 +0100

    alpha 1.3 snapshot 1w5

--- main-js.js
+++ main-js.js
@@ -7,18 +7,9 @@
 // [[ Title ]] //
 var $title = $('title').text();
 console.log('Current page: ' + $title);
-console.log('SNAPSHOT 1w4')
+console.log('SNAPSHOT 1w5')
 
 // [[ Variables ]] //
-var config = {
-    apiKey: "AIzaSyBnrAhY_w3VVnjHc2_UeJJAt2G-FWpAjwM",
-    authDomain: "pwmanager-91c7e.firebaseapp.com",
-    databaseURL: "https://pwmanager-91c7e.firebaseio.com",
-    projectId: "pwmanager-91c7e",
-    storageBucket: "pwmanager-91c7e.appspot.com",
-    messagingSenderId: "200620870742"
-};
-firebase.initializeApp(config);
 
 // ~ Elements ~ //
 var explanation = $('.explanation');
